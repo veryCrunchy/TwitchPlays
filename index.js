23ce983283f016432f40ceacb09408ca2d5bad9f
veryCrunchy <verycrunchydev@gmail.com>
2023-06-04 21:30:53 +0200

    Slight improvements
    - improved auto inputs select
    - improved matching algorithm thingy

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const tmi = require("tmi.js");
+const { Client } = require("tmi.js");
 const { keyboard, Key, getActiveWindow } = require("@nut-tree/nut-js");
-const { distance, closestMatch } = require("closest-match");
+const { findBestMatch } = require("string-similarity");
 const fs = require("fs");
 require("dotenv").config();
-
 const env = {
   CHANNEL: process.env.CHANNEL || "nil",
   MODE: process.env.MODE || "nil",
-  TIMED_INTERVAL: process.env.MODE || "nil",
+  TIMED_INTERVAL: Number(process.env.TIMED_INTERVAL) || 0,
 };
-if (Object.values(env).includes("nil"))
-  return console.error(
-    "\x1b[40m\x1b[31mNot all\x1b[33m .ENV\x1b[31m variables are defined!\x1b[0m\n" +
+if (
+  Object.values(env).includes("nil") ||
+  Object.values(env).includes(0) ||
+  (env.MODE != "CHAOS" && env.MODE != "TIMED")
+)
+  console.error(
+    "\x1b[40m\x1b[31mNot all\x1b[33m .ENV\x1b[31m variables are defined, or are incorrect!\x1b[0m\n" +
       "\x1b[40m\x1b[31mPlease check the\x1b[33m .env\x1b[31m file.\x1b[0m\n" +
       "\x1b[40m\x1b[31mFor guidance, see \x1b[33m.env.example\x1b[31m.\x1b[0m"
-  );
+  ),
+    process.exit();
 
-const client = new tmi.Client({
+const client = new Client({
   channels: [env.CHANNEL],
 });
 
@@ -27,29 +31,25 @@ for (let config of fs
   inputConfigs.push(config);
 }
 if (!inputConfigs.length)
-  return (
-    console.error(
-      "\x1b[0m\x1b[40m\x1b[31mYou do not have any\x1b[33m input\x1b[31m configuration files!\x1b[0m\n" +
-        "\x1b[40m\x1b[31mPlease check the\x1b[33m inputs/README\x1b[31m file.\x1b[0m\n"
-    ),
-    process.exit()
-  );
+  console.error(
+    "\x1b[0m\x1b[40m\x1b[31mYou do not have any\x1b[33m input\x1b[31m configuration files!\x1b[0m\n" +
+      "\x1b[40m\x1b[31mPlease check the\x1b[33m inputs/README\x1b[31m file.\x1b[0m\n"
+  ),
+    process.exit();
 
-////////////////////////////////
-// AUTOMATIC INPUT SWITCHER
-////////////////////////////////
 let lastInput;
 const inputs = new Map();
 const outputs = new Map();
 const times = new Map();
-
-setInput = async () => {
+const interval = setInterval(setInput, 2000);
+async function setInput() {
   const windowRef = await getActiveWindow();
   const [title] = await Promise.all([windowRef.title]);
   if (!title.includes("Dolphin")) return;
   if (lastInput === title) return;
-  let match = closestMatch(title, inputConfigs);
-  const inputData = require(`./inputs/${match}`);
+  const match = findBestMatch(title, inputConfigs);
+  if (match.bestMatch.rating < 0.2) return;
+  const inputData = require(`./inputs/${match.bestMatch.target}`);
   inputs.clear();
   outputs.clear();
   times.clear();
@@ -60,11 +60,14 @@ setInput = async () => {
     outputs.set(i.name, i.outputs);
     times.set(i.name, i.time);
   }
-  lastInput = title;
   console.log(
-    `\x1b[33m -\x1b[0m\x1b[36m Switched to config\x1b[35m ${match}\x1b[32m\x1b[0m`
+    `\x1b[33m -\x1b[0m\x1b[36m ${
+      lastInput ? "Switched to" : "Selected"
+    } config\x1b[35m ${match.bestMatch.target}\x1b[32m\x1b[0m`
   );
-};
+  lastInput = title;
+  clearInterval(interval);
+}
 setInterval(setInput, 15000);
 
 console.log(
@@ -75,24 +78,26 @@ client.connect();
 client.on("connected", async () => {
   setInput();
   console.log(
-    `\x1b[0m\x1b[32mSuccessfully connected to \x1b[35m${env.CHANNEL}`
+    `\x1b[0m\x1b[32mSuccessfully connected to\x1b[35m ${env.CHANNEL}`
   );
   console.log(
-    `\x1b[0m\x1b[32mStarted in \x1b[33m${env.MODE} \x1b[32mmode.\x1b[0m`
+    `\x1b[0m\x1b[32mStarted in \x1b[33m${env.MODE}\x1b[32m mode.\x1b[0m`
   );
 });
 ////////////////////////////////
 // TIMED MODE
 ////////////////////////////////
 if (env.MODE === "TIMED") {
-  tempMap = new Map();
-  tempUser = [];
+  const tempMap = new Map();
+  let tempUser = [];
   client.on("message", async (channel, tags, m, self) => {
-    let { output, name } = getMatch(m);
-    if (!name) return;
-    let map = tempMap.get(name);
+    if (self || !lastInput) return;
+
     let user = tempUser.find((u) => u === tags.username);
     if (user) return;
+    let { name } = getMatch(m);
+    if (!name) return;
+    let map = tempMap.get(name);
     console.log(
       `\x1b[2m\x1b[34m${tags.username} chose:\x1b[33m ${name}\x1b[0m`
     );
@@ -111,12 +116,15 @@ if (env.MODE === "TIMED") {
       windowRef.title,
       windowRef.region,
     ]);
-    if (!title.includes("Dolphin"))
+    if (
+      !title.includes("Dolphin") ||
+      (!title.includes("Dolphin") && !title.includes("|"))
+    )
       return console.log(
         "\x1b[2m\x1b[33mIncorrect window, input preserved\x1b[0m"
       );
     let highestValue = -Infinity;
-    let highestKey = null;
+    let highestKey = "";
     let keysWithHighestValue = [];
     for (const [key, value] of tempMap) {
       if (value > highestValue) {
@@ -134,53 +142,63 @@ if (env.MODE === "TIMED") {
       );
       highestKey = keysWithHighestValue[randomIndex];
     }
-    console.log("\x1b[36mCollective input: \x1b[35m" + highestKey);
+    console.log(`\x1b[36mCollective input:\x1b[35m ${highestKey}\x1b[0m`);
     let time = times.get(highestKey);
     tempMap.clear();
     tempUser = [];
-    hold(Key[outputs.get(highestKey)], time);
-  }, env.TIMED_INTERVAL * 1000);
+
+    const outEnumKey = Key[outputs.get(highestKey)];
+    hold(outEnumKey, time);
+  }, Number(env.TIMED_INTERVAL) * 1000);
 }
 ////////////////////////////////
 // CHAOS MODE
 ////////////////////////////////
 if (env.MODE === "CHAOS") {
   client.on("message", async (channel, tags, m, self) => {
-    if (self) return;
-    console.log(`@${tags.username}: ${m}`);
+    if (self || !lastInput) return;
 
     const windowRef = await getActiveWindow();
-    const [title, region] = await Promise.all([
-      windowRef.title,
-      windowRef.region,
-    ]);
-    if (!title.includes("Dolphin"))
+    const [title] = await Promise.all([windowRef.title]);
+    if (
+      !title.includes("Dolphin") ||
+      (!title.includes("Dolphin") && !title.includes("|"))
+    )
       return console.log(
         "\x1b[2m\x1b[33mIncorrect window, cancelled input.\x1b[0m"
       );
 
-    let { output, time } = getMatch(m);
-
+    let { output, time, name } = getMatch(m);
+    if (!output) return;
+    console.log(`\x1b[34m${tags.username}:\x1b[33m ${name}\x1b[0m`);
     hold(Key[output], time);
   });
 }
 
-hold = async (keys, seconds) => {
+async function hold(keys, seconds) {
   const startTime = Date.now();
   await keyboard.pressKey(keys);
   seconds = seconds * 1000 - (Date.now() - startTime);
   setTimeout(async function () {
     await keyboard.releaseKey(keys);
   }, seconds);
-};
-release = async (keys) => {
-  await keyboard.releaseKey(keys);
-};
-getMatch = (m) => {
+}
+function getMatch(m) {
+  if (typeof m != "string" || m == null)
+    return { output: undefined, time: undefined, name: undefined };
   m = m.trim().toLowerCase();
-  const closest = closestMatch(m, Array.from(inputs.keys()));
-  const name = inputs.get(closest);
-  const time = times.get(name);
+
+  let match = findBestMatch(m, Array.from(inputs.keys()));
+  if (match) {
+    console.log(match.bestMatch.rating);
+    if (match.bestMatch.rating < 0.5)
+      return { output: undefined, time: undefined, name: undefined };
+    match = match.bestMatch.target;
+  } else return;
+
+  const name = inputs.get(match);
+  const time = Number(times.get(name)) || 1;
   const output = outputs.get(name);
+
   return { output: output, time: time, name: name };
-};
+}
